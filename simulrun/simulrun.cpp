e5813afba1c5454236fcff9ed266e0e0ca8d77db
ambiesoft <ambiesoft.trueff@gmail.com>
2021-06-07 16:07:41 +0900

    first

---
+++ simulrun/simulrun.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+#include <Windows.h>
+
+HANDLE ghEventWaitRun;
+int main()
+{
+    HANDLE ghEventWaitRun = CreateEvent(NULL, TRUE, FALSE, NULL);
+
+    std::cout << "Hello World!\n";
+}
